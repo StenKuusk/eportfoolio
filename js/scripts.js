af2f155cf919147c2e906096b576704cb61f2f7e
StenKuusk <sten.kuusk@voco.ee>
2025-02-12 11:51:41 +0200

    Muutsin kontaktide sektsiooni, siis on kohe minu kohta info olemas.

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 600);
     });
     
-    // Sujuv kerimine navbari linkidele
     document.querySelectorAll('nav a').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
