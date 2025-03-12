6441b9e3095a9218e9d939753cd724c4e845d052
StenKuusk <sten.kuusk@voco.ee>
2025-03-12 12:23:07 +0200

    Viimased muudatused! Finally! Nüüd saan tegelema hakata kõige muuga. Panin pildid ja fix'isin probleemid.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ const projects = {
         title: "IT Academy webpage",
         images: [
             'img/it.png',
-            'img/it2.png'
+            'img/it1.png',
+            'img/it2.png',
+            'img/it3.png',
+            'img/it4.png'
         ],
-        description: "Recently we had given a project to make a IT Academy prototype website for our own school. I did this with 1 other person. Due to rules, I cannot give away more previews of it due to it being work in progress. No link to the repo yet as well, so have a melon dog.",
+        description: "Recently we had given a project to make a IT Academy prototype website for our own school. I did this with 1 other person. This project is a webpage for IT Academy, featuring a homepage, specialties, how to apply as an intern, contact info and more to be implemented.",
         tools: "HTML, CSS, JavaScript",
-        repoLink: "https://i.imgur.com/1Rzqi6G.jpeg"
+        repoLink: "https://github.com/StenKuusk/IT-akadeemia-leht",
+        siteLink: "https://itleht.vercel.app"
     },
     project2: {
         title: "Recipe browsing webpage 'Maitsed laual'",
@@ -18,21 +22,22 @@ const projects = {
             'img/recipe3.png',
             'img/recipe4.png'
         ],
-        description: "Me and one of my classmates were tasked to create a project that would use relational databases to sae users info and bring content from the database onto the page. Basically, the user is able to create and log into his own account on our webpage. From there, they can see a random selection of recipes on the homepage or go to the recipes page to filter out and search for some. Clicking on recipes, there is a detailed description for them on how to cook said recipe, what ingrediants are needed and also a nice picture to go along with it. Depending on if the user made a account or not, they can mark the recipe as one of their favorites and also leave comments on various different recipes. All of it being saved into our database.",
+        description: "Me and one of my classmates were tasked to create a project that would use relational databases to save users info and bring content from the database onto the page. Basically, the user is able to create and log into his own account on our webpage. From there, they can see a random selection of recipes on the homepage or go to the recipes page to filter out and search for some. Clicking on recipes, there is a detailed description for them on how to cook said recipe, what ingrediants are needed and also a nice picture to go along with it. Depending on if the user made a account or not, they can mark the recipe as one of their favorites and also leave comments on various different recipes. All of it being saved into our database.",
         tools: "HTML, CSS, JavaScript, MySQL",
         repoLink: "https://github.com/StenKuusk/recipe-repo"
     },
     project3: {
-        title: "Food storage app",
+        title: "Solitaire game",
         images: [
-            'img/toidukapp.png',
-            'img/toidukapp1.png',
-            'img/toidukapp2.png',
-            'img/toidukapp3.png'
+            'img/solitaire.png',
+            'img/solitaire1.png',
+            'img/solitaire2.png',
+            'img/solitaire3.png',
+            'img/solitaire4.png'
         ],
-        description: "This project was made by me and 1 other person. The whole idea we got from UI/UX designers who made a prototype and we had a challenge to try to bring their prototype to life in 2 days. I was sick at the time so I couldn't do much. This is a app, that is supposed to be able to be used by everyone in VOCO. We have our local shared fridge where we put food that is about to expire for others to eat for free. This app is supposed to help us keep track of what is in the fridge, what is about to expire and what is already expired. It also has a feature to add your own food to the fridge and also take food from the fridge.",
+        description: "This project was made solely by me. I was tasked to make something with a authentication system and I was interested in finally making my own game so I thought about a card game. Can't go wrong right? Wrong, it took me a while to get back to work on it cause I was stuck. Once I did, I finally fixed the issue with card functionalities, win conditions (sort of) and style. You can log in or register a account and it will bring you to the homepage. From there, you can press a button to play solitaire, taking you to a seperate page. Solitaire is solitaire, it's self-explanatory. You can move cards around, stack them on top of each other and try to win the game. If you win, it will show a win screen and if you get stuck, well uhh... you can refresh the page to be given a new card set to work with.",
         tools: "HTML, CSS, JavaScript, MySQL, React",
-        repoLink: "https://github.com/StenKuusk/voco-toidukapp-app"
+        repoLink: "https://github.com/StenKuusk/hajus_sisselogimise_leht"
     }
     // Lisa rohkem projekte siia
 };
@@ -57,6 +62,17 @@ function openProject(projectId) {
     document.getElementById('project-description').innerText = project.description;
     document.getElementById('project-tools').innerText = `Tools: ${project.tools}`;
     document.getElementById('project-repo-link').href = project.repoLink;
+    
+    if (project.siteLink) {
+        const siteLinkElement = document.createElement('a');
+        siteLinkElement.href = project.siteLink;
+        siteLinkElement.target = "_blank";
+        siteLinkElement.classList.add('hover-text');
+        siteLinkElement.innerText = "View Project Site";
+        document.getElementById('project-description').appendChild(document.createElement('br'));
+        document.getElementById('project-description').appendChild(siteLinkElement);
+    }
+
     document.getElementById('project-modal').style.display = 'block';
 }
 
